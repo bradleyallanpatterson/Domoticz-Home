2da23b06895258e820fab340205af888a95cac9e
Brad Allan Patterson <bradleyallanpatterson@gmail.com>
2020-05-12 10:50:36 -0700

    Initial version upgrading to 2019

---
+++ DomoticzHome/DomoticzHome/DomoticzHome/ViewModels/MainTabbedPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Prism.Mvvm;
+using Prism.Navigation;
+
+namespace DomoticzHome.ViewModels
+{
+    public class MainTabbedPageViewModel : ViewModelBase
+    { 
+        public MainTabbedPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Settings";
+        }
+    }
+}
